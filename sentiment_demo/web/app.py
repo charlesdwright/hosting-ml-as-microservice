6bee633b4b66fd40a770cbdb90691451953bbb4e
charlesdwright <71340308+charlesdwright@users.noreply.github.com>
2020-10-20 13:49:52 -0400

    synch w/atom

--- sentiment_demo/web/app.py
+++ sentiment_demo/web/app.py
@@ -1,18 +1,23 @@
 from part3 import get_sentiment
 from flask import Flask
 from flask import request
-from flask_cors import CORS
+from flask_cors import CORS, cross_origin
+import datetime
 
 app = Flask(__name__)
-CORS(app)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
+retval ='sentiment_demo_1'
+retval = retval + '<br>' + datetime.datetime.now().replace(microsecond=0).isoformat(' ')
+retval = retval + '<br>' + '<br>'
 
 @app.route('/')
 def hello_whale():
-    return 'Whale, Hello there!'
+    return retval + 'Whale, Hello there!'
 
 @app.route('/blah')
 def blah_yadda():
-    return 'Blah blah yadda yadda'
+    return retval + 'Blah blah yadda yadda'
 
 @app.route('/predict', methods = ['GET', 'POST'])
 def predict():
@@ -22,7 +27,7 @@ def predict():
         input = request.get_json(force=True)['input']
     if not input:
         return 'No input value found'
-    return get_sentiment(input)
+    return retval + get_sentiment(input)
 
 if __name__ == '__main__':
     app.run(debug=False, use_reloader=False, host='0.0.0.0')
